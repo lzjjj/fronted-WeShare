d51c4c1fa3862169e54dea05937552a7fcad0625
louis li <louis.z.j.li@oocl.com>
2019-12-16 16:14:50 +0800

    [louis] Fix detail page ui issue.

--- pages/component/detail.vue
+++ pages/component/detail.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
-		<view class="image" style="margin: 10px;">
-			<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg" mode="widthFix"></image>
+		<view style="margin: 10px;">
+			<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg" mode="widthFix" style="width: 100%;"></image>
 			<view class="detail-content text-cut"><text class="label">话题：</text>DDD Event Storming</view>
 			<view class="detail-content text-cut"><text class="label">演讲者：</text>Jay Zhou</view>
 			<view class="detail-content text-cut"><text class="label">时间：</text>2019-12-04 00：00</view>
@@ -18,15 +18,17 @@
 </script>
 
 <style>
-	.detail-content{
+	.detail-content {
 		font-size: 0.9rem;
 		margin-top: 10px;
 	}
-	.label{
+
+	.label {
 		font-weight: bold;
 		margin-top: 10px;
 	}
-	.apply-button{
+
+	.apply-button {
 		margin-top: 20px;
 		width: 100%;
 		height: 2.5rem;
