b189bf061f22b435c84ed611f654b30df014f328
XUVI7 <vic.xu@oocl.com>
2019-12-17 16:37:18 +0800

    [VIC] global photo url

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const globalUrl = 'http://kuangal2-w10.corp.oocl.com:3000'
+const globalPhotoUrl = 'http://xuvi7-w10:9090/'
 const requestUrls = {
   getTopics: globalUrl + '/home/topics', //Get topic list
   getRewards: globalUrl + '/rewards',
