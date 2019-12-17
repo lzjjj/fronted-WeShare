94e939cc3243c84264a4e1d895ca5bc43fa3ed25
louis li <louis.z.j.li@oocl.com>
2019-12-17 17:07:13 +0800

    [louis] code merge issue.

--- api.js
+++ api.js
@@ -1,15 +1,15 @@
 const globalUrl = 'http://kuangal2-w10.corp.oocl.com:3000'
 const globalPhotoUrl = 'http://xuvi7-w10:9090/'
 const requestUrls = {
-  getTopics: globalUrl + '/home/topics', //Get topic list
-  getRewards: globalUrl + '/rewards',
-  login: globalUrl + '/login',
-  balance: globalUrl + '/user/balance',
-  getMyJoins: globalUrl + '/home/topics/joiner/1', //Get myJoins list
-  getMyCreates: globalUrl + '/home/topics/owner/1', //Get mycreate list
-  record: globalUrl + '/user/records',
-  picUpload: "http://xuvi7-w10:3000/multerUpload/upload",
-  WeChatlogin: globalUrl + '/login/'
-  picLoad: "http://xuvi7-w10:9090"
+	getTopics: globalUrl + '/home/topics', //Get topic list
+	getRewards: globalUrl + '/rewards',
+	login: globalUrl + '/login',
+	balance: globalUrl + '/user/balance',
+	getMyJoins: globalUrl + '/home/topics/joiner/1', //Get myJoins list
+	getMyCreates: globalUrl + '/home/topics/owner/1', //Get mycreate list
+	record: globalUrl + '/user/records',
+	picUpload: "http://xuvi7-w10:3000/multerUpload/upload",
+	WeChatlogin: globalUrl + '/login/',
+	picLoad: "http://xuvi7-w10:9090"
 }
 export default requestUrls
