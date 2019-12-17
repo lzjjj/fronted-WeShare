4be7dd07ebd79a4571da75d0271bdb447249b6bb
louis li <louis.z.j.li@oocl.com>
2019-12-17 14:27:45 +0800

    [louis] Update api host.

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const globalUrl = 'http://tangtr2-w10.corp.oocl.com:3000'
+const globalUrl = 'http://kuangal2-w10.corp.oocl.com:3000'
 const requestUrls = {
   getTopics: globalUrl + '/home/topics', //Get topic list
   getRewards: globalUrl + '/rewards',
