5cea4a5e628d6cfa05064ba373b31dde58dd6f70
XUVI7 <vic.xu@oocl.com>
2019-12-18 16:17:28 +0800

    [VIC] dialog

--- pages/component/reward_dialog_modal.vue
+++ pages/component/reward_dialog_modal.vue
@@ -7,7 +7,7 @@
 					<text class="cuIcon-close text-red"></text>
 				</view>
 			</view>
-			<view v-if="msg==''" class="padding-xl">
+			<view v-if="msg==''" class="padding-xl bg-white">
 				<view class="flex p-xs margin-bottom-sm mb-sm">
 					<view class="flex-sub padding-sm"><view class="align-center text-bold" style="height: 35px; display: flex;">兑换数量：</view></view>
 					<view class="flex-twice padding-sm"><uni-number-box ref="numberbox" :value="0" :max="100" @change="bindChange"></uni-number-box></view>
