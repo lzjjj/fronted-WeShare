1f95fe08896ee0c1e188d627b4d343279b109a07
louis li <louis.z.j.li@oocl.com>
2019-12-19 16:44:12 +0800

    [louis] fix issue.

--- pages/component/detail.vue
+++ pages/component/detail.vue
@@ -38,7 +38,7 @@
 			<button v-if="type == 0 && topic.registration_is_valid" class="bg-grey cu-btn apply-button">已报名</button>
 			<button v-if="type == 1" class="bg-gradual-blue cu-btn apply-button" @click="cancelRegister">取消报名</button>
 			<view v-if="type == 2" class="flex justify-around">
-				<button class="bg-gradual-blue cu-btn apply-button" style="margin-right: 5%;" @click="cancelTopic">取消分享</button>
+				<button v-if= "topic.status == 'new'" class="bg-gradual-blue cu-btn apply-button" style="margin-right: 5%;" @click="cancelTopic">取消分享</button>
 				<button v-if="false" class="bg-gradual-blue cu-btn apply-button">编辑</button>
 			</view>
 			<rewardDialog ref="popup" :title="title" :msg="msg" @confirm='popupConfirm'></rewardDialog>
@@ -116,7 +116,7 @@
 							topic_id: this.topic_id
 						}
 					}).then((res) => {
-						if (res.msg == REGISTRATION_SUCCESS) {
+						if (res.status) {
 							this.$refs.message.success("报名成功");
 							setTimeout(uni.reLaunch, 1000, {url: '../index/index'})
 						} else {
@@ -154,7 +154,7 @@
 						this.$refs.message.sucess("取消分享会成功")
 					});
 				}
-				setTimeout(uni.reLaunch, 1000, {url: '../index/index?TabCur=1'})
+				setTimeout(uni.reLaunch, 1000, {url: '../index/index?TabCur=' + this.$props.type})
 			}
 		}
 	}
