689c0c4eaf9e034b41310a305730355def46860c
louis li <louis.z.j.li@oocl.com>
2019-12-20 15:49:41 +0800

    [louis] fix issue.

--- pages/component/detail.vue
+++ pages/component/detail.vue
@@ -36,7 +36,7 @@
 			</view>
 			<button v-if="type == 0 && !topic.registration_is_valid" class="bg-gradual-blue cu-btn apply-button" @click="register">报名</button>
 			<button v-if="type == 0 && topic.registration_is_valid" class="bg-grey cu-btn apply-button">已报名</button>
-			<button v-if="type == 1" class="bg-gradual-blue cu-btn apply-button" @click="cancelRegister">取消报名</button>
+			<button v-if="type == 1 && (topic.status=='new' || topic.status=='deadline')" class="bg-gradual-blue cu-btn apply-button" @click="cancelRegister">取消报名</button>
 			<view v-if="type == 2" class="flex justify-around">
 				<button v-if="topic.status == 'new'" class="bg-gradual-blue cu-btn apply-button" style="margin-right: 5%;" @click="cancelTopic">取消分享</button>
 				<button v-if="false" class="bg-gradual-blue cu-btn apply-button">编辑</button>
